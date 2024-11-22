65429b46d4f0c1ef5177abc4a4e2859c739d796e
Ireneusz Przystaś <irrk@op.pl>
2024-11-22 09:42:37 +0100

    poprawki

--- pierwsze c/zad4_a.cpp
+++ pierwsze c/zad4_a.cpp
@@ -2,14 +2,14 @@
 int main()
 {
     int i, potega, do_jakiej_potegi;
-    printf("Podaj do jakiej potęgi: ");
+    printf("Podaj do jakiej potegi: ");
     scanf("%i", &do_jakiej_potegi);
     potega = 1;
     i = 1;
     while (i <= do_jakiej_potegi)
     {
-        printf("%i\n", potega);
         potega = potega * 2;
+        printf("%i\n", potega);
         i = i + 1;
     }
     return 0;
