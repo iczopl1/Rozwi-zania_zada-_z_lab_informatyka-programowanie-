308d7a21c0c3634746d943cd6dadf870a681320d
Ireneusz Przystaś <irrk@op.pl>
2024-11-22 08:42:16 +0100

    nice

--- pierwsze c/zad4_a.cpp
+++ pierwsze c/zad4_a.cpp
@@ -3,7 +3,7 @@ int main()
 {
     int i, potega, do_jakiej_potegi;
     printf("Podaj do jakiej potęgi: ");
-    scanf("%i", &do_jaiej_potegi);
+    scanf("%i", &do_jakiej_potegi);
     potega = 1;
     i = 1;
     while (i <= do_jakiej_potegi)
